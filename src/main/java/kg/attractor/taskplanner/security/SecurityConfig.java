76fae561114ee52d371b97997126fe750793580a
urmat_mederbekov <urmat.mederbekov@gmail.com>
2020-03-28 22:39:38 +0600

    security for authorizing added

---
+++ src/main/java/kg/attractor/taskplanner/security/SecurityConfig.java
@@ -0,0 +1,43 @@
+package kg.attractor.taskplanner.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder encoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests()
+                .antMatchers("/users/**", "/tasks/**")
+                .hasRole("USER")
+                .antMatchers("/")
+                .permitAll()
+                .anyRequest()
+                .permitAll();
+// Настраиваем хранение сессий. Не храним сессию.
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+// Используем авторизацию по механизму Http Basic.
+// Данные пользователя передаются через заголовок запроса
+        http.httpBasic();
+// Так как мы авторизуемся через заголовок запроса, то
+// форма входа на сайт и выхода с него нам тоже не нужны.
+        http.formLogin().disable().logout().disable();
+// Так как у нас REST сервис, нам не нужна защита от CSRF
+        http.csrf().disable();
+    }
+
+}
