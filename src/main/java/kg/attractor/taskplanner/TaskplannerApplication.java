7504974e87cbbe0df4b3e1b0e50bb3e6b97dcadb
urmat_mederbekov <urmat.mederbekov@gmail.com>
2020-03-28 22:45:30 +0600

    application added

---
+++ src/main/java/kg/attractor/taskplanner/TaskplannerApplication.java
@@ -0,0 +1,13 @@
+package kg.attractor.taskplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskplannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskplannerApplication.class, args);
+    }
+
+}
